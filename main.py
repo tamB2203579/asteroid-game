4567c08195e05bfb2fc12f56f0ca8d9455d845be
tamb2203579 <tamb2203579@student.ctu.edu.vn>
2025-08-17 13:52:14 +0000

    2. add player obj for the game

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+    dt = 0
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updatable.update(dt)    
+        
         screen.fill(color="black")
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+
+        # limit frame rate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
